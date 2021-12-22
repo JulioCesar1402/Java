c24c4f7ef9f1a463718e359275152dc1ae408343
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-12-22 09:01:58 -0300

    feat: métodos, construtores e controles de acesso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import tarefas.Tarefa;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Minhas Tarefas");
+    System.out.println("-----------------");
+
+    Tarefa tarefa1 = new Tarefa("regar as plantas");
+    tarefa1.exibirTarefa();
+    System.out.println("A tarefa tem " + tarefa1.obterTamanhoTarefa() + " caracteres");
+  }
 }
