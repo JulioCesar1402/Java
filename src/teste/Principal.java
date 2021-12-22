9cdc740ce4e341bd92bd005c7647ae2b7cb96dc4
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-12-22 09:00:46 -0300

    feat: hello world

---
+++ src/teste/Principal.java
@@ -0,0 +1,11 @@
+package teste;
+
+import yeste2.Principal2;
+
+public class Principal {
+  public static void main(String[] args) {
+
+    Principal2 principal2 = new Principal2();
+
+  }
+}
---
+++ src/yeste2/Principal2.java
@@ -0,0 +1,11 @@
+package yeste2;
+
+public class Principal2 {
+  public static void main(String[] args) {
+
+    String nome = "Monts";
+
+    System.out.println("hello " + nome);
+
+  }
+}
